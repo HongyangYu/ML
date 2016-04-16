d73cfba0c061454343bc575b3ae8ce42ac9478c5
Hongyang <hongyangyu.henry@gmail.com>
2016-04-15 21:38:03 -0400

    Update AttributesProcess.py

--- AttributesProcess.py
+++ AttributesProcess.py
@@ -9,10 +9,12 @@ Created on Fri Apr 15 13:33:28 2016
 import json
 class Attributes:
     def getAttributesList(self, filename, dic):
-        _file = file(filename, 'r') 
+        _file = open(filename, 'r') 
+        i = 0
         for line in _file:
             if line == "\n":
                 continue
+            i = i+1
             words = line.split(",")
             words[1] = words[1].strip('"')
             words[2] = words[2].strip('"\n')
@@ -21,6 +23,8 @@ class Attributes:
             else:
                 product_list = [(words[0],words[2])]
                 dic[words[1]] = product_list
+        _file.close()
+        print "lines of cvs file", i
         return dic  
         
 
@@ -29,7 +33,12 @@ attr = Attributes()
 dic = {} # name: (product_id,value)
 dic = attr.getAttributesList("attributes.csv", dic)
 
+if "" in dic:
+    del dic[""]
+    
+print "number of dic keys =", len(dic)
 with open('attributes_data.json', 'w') as fp:
     json.dump(dic, fp)
-print("END")
 
+
+print("END")
